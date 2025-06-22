54d546907b9449137da20533343b8e6380a12802
juhilxbhatt <100752923+juhilxbhatt@users.noreply.github.com>
2025-06-22 19:50:17 +1000

    init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ flask-server/README.md
@@ -0,0 +1,8 @@
+To Run Flask-Server
+
+
+Create venv: python3 -m venv venv
+
+Run venv: source venv/bin/activate
+
+Install Requirements: pip3 install -r requirements.txt
