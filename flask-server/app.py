80f5c2ef5ee187e366d9e72c5dae1d48a30af4f5
juhilxbhatt <100752923+juhilxbhatt@users.noreply.github.com>
2025-06-25 20:38:51 +1000

    Trying to use Render

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+from os import environ
 
+load_dotenv()
 app = Flask(__name__)
 cors = CORS(app, origins='*')
+
+app.config['DEBUG'] = environ.get('FLASK_DEBUG')
 
 @app.route('/ping')
 def ping():
     return jsonify({'message': 'pong'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
